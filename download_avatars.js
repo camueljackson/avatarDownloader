1ddebcc55554e70fe263e4d02af3e10bdb8254b8
camueljackson <camille.a.pelletier@gmail.com>
2018-06-11 19:36:46 +0000

    able to download image if path hardcoded

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
-var request = require('request');
+var request   = require('request');
+const fs      = require('fs');
 const secrets = require('./secrets.js')
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -13,20 +15,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': 'GITHUB_TOKEN'
     }
   };
-
   request(options, function(err, res, body) {
     let parseResult = JSON.parse(body);
     cb(err, parseResult)
   });
-
 }
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (err) {
+            throw err;
+          })
+         .on('response', function (response) {
+           console.log('Response Status Code: ', response.statusCode);
+         })
+         .pipe(fs.createWriteStream('./downloaded.jpg'));
+}
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-result.forEach(function (element) {
-  console.log(element.avatar_url)
-})
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
+  result.forEach(function (element) {
+    console.log(element.avatar_url)
+  })
 });
 
 
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
